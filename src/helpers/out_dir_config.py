84c5731b9802b7c6b06ff550ca2347434c147d9b
Andreas <kaufmann-a@hotmail.ch>
2021-11-06 14:58:19 -0700

    remove mkdir for out_dir path

--- src/helpers/out_dir_config.py
+++ src/helpers/out_dir_config.py
@@ -8,6 +8,4 @@ def get_out_dir(calling_file=''):
     exp_name = os.path.basename(calling_file)[:-3]
     date_string = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
     new_dir = os.path.join(out_dir, date_string + "_" + exp_name)
-    if not os.path.exists(new_dir):
-        os.makedirs(new_dir)
     return new_dir
