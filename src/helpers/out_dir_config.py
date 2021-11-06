938762fa18aeb0fa25bd7d8f89f3bf45ff208d4a
braunjon <25931090+braunjon@users.noreply.github.com>
2021-11-06 22:56:42 +0100

    new default: do not create out dir

--- src/helpers/out_dir_config.py
+++ src/helpers/out_dir_config.py
@@ -2,12 +2,14 @@ import os
 
 from datetime import datetime
 
-def get_out_dir(calling_file=''):
+
+def get_out_dir(calling_file='', create_dir=False):
     # Out-dir from env var
     out_dir = os.getenv("OUT_DIR")
     exp_name = os.path.basename(calling_file)[:-3]
     date_string = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
     new_dir = os.path.join(out_dir, date_string + "_" + exp_name)
-    if not os.path.exists(new_dir):
-        os.makedirs(new_dir)
+    if create_dir:
+        if not os.path.exists(new_dir):
+            os.makedirs(new_dir)
     return new_dir
