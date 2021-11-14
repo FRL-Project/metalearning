a3e5118f038757545e13de84e350e7be5e2cbfd5
braunjon <25931090+braunjon@users.noreply.github.com>
2021-11-14 15:47:39 +0100

    update continue experiment with env. variable

--- src/continue_experiment.py
+++ src/continue_experiment.py
@@ -2,11 +2,17 @@ import click
 from garage import Trainer, wrap_experiment
 from garage.experiment.deterministic import set_seed
 
+from helpers import out_dir_config
+from helpers import environmentvariables
+
+# Init env. variables
+environmentvariables.initialize()
+
 
 @click.command()
 @click.option('--seed', default=1)
 @click.option('--resume_from_dir', default='')  # e.g. "./data/local/experiment/maml_trpo_metaworld_ml10"
-@wrap_experiment
+@wrap_experiment(snapshot_mode='all', log_dir=out_dir_config.get_out_dir(__file__))
 def continue_training(ctxt, seed=1, resume_from_dir=""):
     set_seed(seed)
     trainer = Trainer(ctxt)
