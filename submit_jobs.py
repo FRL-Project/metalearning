ccfe73da800e711856c4083cb796142efae8209a
Andreas <kaufmann-a@hotmail.ch>
2021-12-01 09:29:57 +0100

    Added all tasks for sac exps

--- submit_jobs.py
+++ submit_jobs.py
@@ -9,14 +9,16 @@ import click
 algos_file = {
     # algo name - file name component
     "maml": 'maml_trpo',
-    "pearl": 'pearl'
+    "pearl": 'pearl',
+    "sac": 'sac'
 }
 
 envs_file = {
     # env name - file name component
     "ml1": 'metaworld_ml1_basketball.py',
     "ml10": 'metaworld_ml10.py',
-    "ml45": 'metaworld_ml45.py'
+    "ml45": 'metaworld_ml45.py',
+    "-": 'metaworld'
 }
 
 experiments_params = {
@@ -39,8 +41,25 @@ experiments_params = {
             "lr": ['7e-4', '3e-4', '1e-4'],
             "use_gpu": [True]
         }
+    },
+    "sac": {
+        "env_name": {
+            "env_name":
+                [
+                    'bin-picking-v2', 'box-close-v2', 'button-press-topdown-v2', 'button-press-topdown-wall-v2',
+                    'button-press-v2', 'button-press-wall-v2', 'coffee-button-v2', 'coffee-pull-v2',
+                    'coffee-push-v2', 'dial-turn-v2', 'disassemble-v2', 'door-close-v2', 'door-lock-v2',
+                    'door-open-v2', 'door-unlock-v2', 'hand-insert-v2', 'drawer-close-v2', 'drawer-open-v2',
+                    'faucet-open-v2', 'faucet-close-v2', 'hammer-v2', 'handle-press-side-v2', 'handle-press-v2',
+                    'handle-pull-side-v2', 'handle-pull-v2', 'lever-pull-v2', 'peg-insert-side-v2',
+                    'pick-place-wall-v2',
+                    'pick-out-of-hole-v2', 'reach-v2', 'push-back-v2', 'push-v2', 'pick-place-v2', 'plate-slide-v2',
+                    'plate-slide-side-v2', 'plate-slide-back-v2', 'plate-slide-back-side-v2', 'peg-unplug-side-v2',
+                    'soccer-v2', 'stick-push-v2', 'stick-pull-v2', 'push-wall-v2', 'reach-wall-v2', 'shelf-place-v2',
+                    'sweep-into-v2', 'sweep-v2', 'window-open-v2', 'window-close-v2'
+                ]
+        }
     }
-
 }
 
 
@@ -64,6 +83,10 @@ def submit_job(algo, env, exp, time, n_cpus, mem, gpu=None, path="./src"):
     python_file_name = algos_file[algo] + "_" + envs_file[env]
     python_file_path = os.path.join(path, python_file_name)
 
+    # if python_file_name == 'sac_metaworld':
+    #     tasks_list = open("./cfg_files/tasks", "r")
+    #     experiment_parameter = tasks_list.read().split("\n")
+    # else:
     experiment_parameter = experiments_params[algo][exp]
 
     for params in product_dict(**experiment_parameter):
