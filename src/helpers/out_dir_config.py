9139bc9e8ccd1f697dbb0e4c53ae1c5b5e1674eb
braunjon <25931090+braunjon@users.noreply.github.com>
2021-12-06 00:06:56 +0100

    add postfix option to folder name

--- src/helpers/out_dir_config.py
+++ src/helpers/out_dir_config.py
@@ -3,11 +3,17 @@ import os
 from datetime import datetime
 
 
-def get_out_dir(calling_file=''):
+def get_out_dir(calling_file='', postfix=None):
     # Out-dir from env var
     out_dir = os.getenv("OUT_DIR")
     exp_name = os.path.basename(calling_file)[:-3]
     date_string = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
-    new_dir = os.path.join(out_dir, date_string + "_" + exp_name)
-    return new_dir
 
+    folder_name = date_string + "_" + exp_name
+
+    if postfix is not None:
+        folder_name += "_" + postfix
+
+    new_dir = os.path.join(out_dir, folder_name)
+
+    return new_dir
